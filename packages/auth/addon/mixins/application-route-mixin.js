2a63d3b075d0984f78b7e5f0d4bc1c651be7c8d7
gabrielcorado <gabrielcorado@mail.com>
2015-12-03 10:55:13 -0200

    YeboApplicationRouteMixin

---
+++ packages/auth/addon/mixins/application-route-mixin.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+
+/**
+ * Yebo Route Mixin
+ *
+ */
+export default ApplicationRouteMixin;
