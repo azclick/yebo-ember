28a7f71c93d471677a0158038f2fdf419540145e
gabrielcorado <gabrielcorado@mail.com>
2015-12-21 10:44:24 -0200

    Search the products by the text recieved

--- packages/storefront/app/routes/yebo/products/index.js
+++ packages/storefront/app/routes/yebo/products/index.js
@@ -28,12 +28,24 @@ export default Ember.Route.extend({
   currentQuery: null,
 
   model: function(params) {
-    console.log(params);
+    // Start a new Query if its necessary
+    if( this.get('currentQuery') === null )
+      this.set('currentQuery', new YeboSDK.Products());
+
+    // Query
+    let query = this.get('currentQuery');
+
+    // Set the search
+    // @todo Do not send seach param if its not necessary
+    if( params.search === 'null' )
+      query.search(null);
+    else
+      query.search(params.search);
 
     // Make the searches
     return Ember.RSVP.hash({
       // products: this.yebo.store.findAll('product'),
-      products: this.get('yebo.products').search(),
+      products: this.get('yebo.products').search(query),
       taxonomies: this.yebo.store.findAll('taxonomy')
     });
   }
