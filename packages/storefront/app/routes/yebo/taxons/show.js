71b55918db68d6da97a69c955892c03134cd6b86
gabrielcorado <gabrielcorado@mail.com>
2016-01-04 14:20:43 -0200

    Taxons show route now shows the products related to the current taxon

---
+++ packages/storefront/app/routes/yebo/taxons/show.js
@@ -0,0 +1,72 @@
+import Ember from 'ember';
+
+/**
+ * Taxons show page
+ * This page shows products related to the current taxon
+ */
+export default Ember.Route.extend({
+  // Define the query params
+  queryParams: {
+    page: {}
+  },
+
+  //
+  model: function(params) {
+    // Query and rule for this route
+    let query, rule;
+
+    // Check if the query exists
+    if( !this.get('currentQuery') ) {
+      // Define a query
+      query = new YeboSDK.Products();
+
+      // Define the rule
+      // @todo Keep it in the and rule
+      rule = new YeboSDK.Products.Rules.taxonomy([], 'permalink', 'or');
+
+      // Add the rule to the query
+      query.and(rule);
+
+      // Define the number of results per page
+      query.perPage(15);
+
+      // Set query and rule to the route
+      this.set('currentQuery', query);
+      this.set('currentRule', rule);
+    } else {
+      // Get the current query and rule
+      query = this.get('currentQuery');
+      rule = this.get('currentRule');
+    }
+
+    // Set a new value to the rule
+    rule.values = params.taxon.split('/');
+
+    // Set the page
+    query.page(params.page);
+
+		// Todo: Reuse taxonomy find all
+    return Ember.RSVP.hash({
+      taxonomies: this.yebo.store.findAll('taxonomy'),
+      taxon: this.yebo.store.find('taxon', params.taxon),
+      products: this.yebo.products.search(query),
+    });
+  },
+
+  // Change the current controller
+  setupController: function(controller, model) {
+    // This is indispensable, if out, the model won't be passed to the view
+    controller.set('model', model);
+
+    // the component that requires current taxon and taxonomies is in application
+    let appController = this.controllerFor('application');
+
+    // Define some values
+    let taxon = model.taxon;
+    let taxonomies = model.taxonomies;
+
+    // Set the values to the application controller
+    appController.set('currentTaxonomy', taxon.get('taxonomy'));
+    appController.set('taxonomies', taxonomies);
+  }
+});
